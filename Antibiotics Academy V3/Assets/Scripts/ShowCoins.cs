df086a46ac3fb48492b1d4c22269ce5fe9d71f23
Yeo-Yong-Jun <69186993+Yeo-Yong-Jun@users.noreply.github.com>
2020-11-23 14:25:20 +0800

    display player's coins in mainhub and match 3 minigame, show antibiotic costs 40 coins

---
+++ Antibiotics Academy V3/Assets/Scripts/ShowCoins.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowCoins : MonoBehaviour
+{
+    Text text;
+
+    void Start()
+    {
+        text = this.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = "Coins: " + Player.coins;
+    }
+}
