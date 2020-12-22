7d7375c767556268ef2ddceb584c8691cae40c03
Yeo-Yong-Jun <69186993+Yeo-Yong-Jun@users.noreply.github.com>
2020-12-22 14:51:52 +0800

    updated login function

--- Antibiotics Academy V3/Assets/AA Database/Login.cs
+++ Antibiotics Academy V3/Assets/AA Database/Login.cs
@@ -20,32 +20,94 @@ public class Login : MonoBehaviour
 
     public void CallLogin()
     {
-        //StartCoroutine(LoginPlayer());
+        StartCoroutine(LoginPlayer());
         //StartCoroutine(GetRequest("http://103.239.222.212/ALIVEService/api/login/Generate?username=player1&password=2020Alive"));
-        StartCoroutine(PostRequest());
+        //StartCoroutine(PostRequest());
         //LoginPlayers();
     }
 
+    //IEnumerator LoginPlayer()
+    //{
+    //    WWWForm form = new WWWForm();
+    //    form.AddField("name", nameField.text);
+    //    form.AddField("password", passwordField.text);
+    //    //WWW www = new WWW("http://localhost/sqlconnect/login.php", form);
+    //    WWW www = new WWW("http://103.239.222.212/ALIVEService/api/login/Generate?username=player1&password=2020Alive", form);
+    //    yield return www;
+    //    if (www.text[0] == '0')
+    //    {
+    //        DBManager.username = nameField.text;
+    //        //DBManager.score = int.Parse(www.text.Split('\t')[1]);
+    //        SceneManager.LoadScene(12);
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("User login failed. Error #" + www.text);
+    //    }
+    //}
+
     IEnumerator LoginPlayer()
     {
-
         WWWForm form = new WWWForm();
-        form.AddField("name", nameField.text);
-        form.AddField("password", passwordField.text);
-        //WWW www = new WWW("http://localhost/sqlconnect/login.php", form);
-        WWW www = new WWW("http://103.239.222.212/ALIVEService/api/login/Generate?username=player1&password=2020Alive", form);
+        WWW www = new WWW("http://103.239.222.212/ALIVE2Service/api/login/Generate?username=" + nameField.text + "&password=" + passwordField.text, form);
         yield return www;
-        if (www.text[0] == '0')
+
+        Debug.Log(www.text);
+        Debug.Log(www.error);
+        Debug.Log(www.url);
+
+        currentLogin = System.DateTime.Now;
+
+        string path = Application.persistentDataPath + "/PlayerSave.json";
+        
+        if(www.error == null)
         {
-            DBManager.username = nameField.text;
-            //DBManager.score = int.Parse(www.text.Split('\t')[1]);
-            SceneManager.LoadScene(12);
+            Debug.Log("Login Successful!");
+            if (File.Exists(path))
+            {
+                Player.Load();
+                if (currentLogin >= lastLogin.AddHours(18))
+                {
+                    Player.coins += 10;
+                    Debug.Log("You got 10 coins for logging in today!");
+                    Player.lives += 3;
+                    Debug.Log("You got 3 lives for logging in today!");
+                }
+                else
+                {
+                    Debug.Log("You have already logged in for the day!");
+                }
+            }
+            else
+            {
+                Player.coins += 10;
+                Debug.Log("You got 10 coins for logging in today!");
+                Player.lives += 3;
+                Debug.Log("You got 3 lives for logging in today!");
+            }
+
+            lastLogin = currentLogin;
+
+            if (Player.donePreQuiz == false)
+            {
+                SceneManager.LoadScene(12);
+                Player.donePreQuiz = true;
+            }
+            else if (Player.donePreQuiz == true)
+            {
+                SceneManager.LoadScene("Cutscene");
+            }
+            else
+            {
+                SceneManager.LoadScene("Cutscene");
+            }
         }
         else
         {
-            Debug.Log("User login failed. Error #" + www.text);
+            Debug.Log("Login Failed!");
         }
     }
+
     //IEnumerator GetRequest(string uri)
     //{
     //    using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
