2f5959ce24233a2cb3ceeb89b2e0fef16ad10834
TrixyL <trixyloh@gmail.com>
2021-01-31 02:38:31 +0800

    Edit layout of community and hospital + camera movement
    Combine hospital with rest of the community
    Contain player movement within set space
    Implement camera movement to stop when borders of the space are reached

--- Antibiotics Academy V3/Assets/AA MainHub/Scripts/CameraController.cs
+++ Antibiotics Academy V3/Assets/AA MainHub/Scripts/CameraController.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public Transform player;
+
+    public BoxCollider2D LockedLeft;
+    public BoxCollider2D LockedRight;
+    public BoxCollider2D LockedUp;
+    public BoxCollider2D LockedDown;
+
+    Camera cam;
+
+    float camOrthSize;
+    float camHorizontalOrth;
+    
+    float horizontal;
+    float vertical;
+    
     // Start is called before the first frame update
     void Awake()
     {
-        GetComponent<UnityEngine.Camera>().orthographicSize = 6; // set the orthographic size of the camera to 6
+        cam = GetComponent<UnityEngine.Camera>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camOrthSize = cam.orthographicSize = 6; // set the orthographic size of the camera to 6
+        camHorizontalOrth = camOrthSize * cam.aspect; //get width of current viewing volume
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Mathf.Clamp(player.position.x, LockedLeft.bounds.max.x + camHorizontalOrth, LockedRight.bounds.min.x - camHorizontalOrth); //set horizontal min and max range for camera movement
+        vertical = Mathf.Clamp(player.position.y + 2f, LockedDown.bounds.max.y + camOrthSize, LockedUp.bounds.min.y - camOrthSize); //set vertical min and max range for camera movement
+        transform.position = new Vector3(horizontal, vertical, transform.position.z);
     }
 
 }
--- Antibiotics Academy V3/Assets/AA MainHub/Scripts/Teleport.cs
+++ Antibiotics Academy V3/Assets/AA MainHub/Scripts/Teleport.cs
@@ -10,5 +10,11 @@ public class Teleport : MonoBehaviour
     {
         // if player collide with the game object this script is attached to, the player would get teleported to game object A's position
         collision.gameObject.transform.position = A.transform.position;
+
+        if(gameObject.name == "Hospital") //when player leaves community (enters hospital)
+        {
+            GameObject.Find("Main Camera").GetComponent<CameraController>().enabled = false; //disable camera controller script
+        }
+        else GameObject.Find("Main Camera").GetComponent<CameraController>().enabled = true; //enable camera controller script
     }
 }
