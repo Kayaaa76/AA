b227afa13f3599ead854bedd2a1f0ba09af0b78c
TrixyL <trixyloh@gmail.com>
2021-02-25 19:44:46 +0800

    Implement pause feature for minigame
    Enable pause, go back to levels, or quit game(match 3)

---
+++ Antibiotics Academy V3/Assets/AA Match3/Scripts/UI Scripts/PauseGame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject levelsConfirmMenu;
+    public GameObject quitConfirmMenu;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void resume()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void confirmLevels()
+    {
+        levelsConfirmMenu.SetActive(true);
+    }
+
+    public void undoLevels()
+    {
+        levelsConfirmMenu.SetActive(false);
+    }
+
+    public void confirmQuit()
+    {
+        quitConfirmMenu.SetActive(true);
+    }
+
+    public void undoQuit()
+    {
+        quitConfirmMenu.SetActive(false);
+    }
+}
